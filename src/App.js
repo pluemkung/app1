d348cb22157b19ccea4fb908d6d2b6158edcba44
pluemkung <39697097+pluemkung@users.noreply.github.com>
2025-12-18 11:57:36 +0700

    divStyle
    divStyle

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <table border="1">
-      <th>Product</th><th>Price</th>
-      <tr> 
-        <td>React</td>
-        <td>320</td>
-      </tr>
-      <tr>
-        <td>React Native</td>
-        <td>340</td>
-      </tr>
-    </table>
-  );
+  const divStyle = {
+    color: 'red',
+    backgroundColor: 'powderblue',
+    fontSize: 'large',
+  };
+  return <div style={divStyle}>Hello React </div>;
+  // (
+  //   <table border="1">
+  //     <th>Product</th><th>Price</th>
+  //     <tr> 
+  //       <td>React</td>
+  //       <td>320</td>
+  //     </tr>
+  //     <tr>
+  //       <td>React Native</td>
+  //       <td>340</td>
+  //     </tr>
+  //   </table>
+  // );
 }
 
 export default App;
