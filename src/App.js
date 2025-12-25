b9f9018695e16e56cc4a0ba8537e3141817e7a3d
pluemkung <39697097+pluemkung@users.noreply.github.com>
2025-12-25 09:44:37 +0700

    style css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import React from 'react';
 
 function App() {
-  const divStyle = {
-    color: 'red',
-    backgroundColor: 'powderblue',
-    fontSize: 'large',
-  };
-  return <div style={divStyle}>Hello React </div>;
+  return (
+    <>
+      <h2>DeveloperThai.com</h2>
+      <div className='title'>React & React Native</div>
+    </>
+  )
+  // const divStyle = {
+  //   color: 'red',
+  //   backgroundColor: 'powderblue',
+  //   fontSize: 'large',
+  // };
+  // return <div style={divStyle}>Hello React </div>;
   // (
   //   <table border="1">
   //     <th>Product</th><th>Price</th>
