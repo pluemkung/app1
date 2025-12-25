3764c7823159eb94f72f9278c826b84dc3a50334
pluemkung <39697097+pluemkung@users.noreply.github.com>
2025-12-25 11:11:13 +0700

    func-components
    func-components

--- src/App.js
+++ src/App.js
@@ -2,33 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import './style.css';
 import React from 'react';
+import { Content, Footer, Header } from './func-components';
 
 function App() {
   return (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
+     <Header/>,
+     <Content/>,
+     <Footer/>
     </>
+   
   )
-  // const divStyle = {
-  //   color: 'red',
-  //   backgroundColor: 'powderblue',
-  //   fontSize: 'large',
-  // };
-  // return <div style={divStyle}>Hello React </div>;
-  // (
-  //   <table border="1">
-  //     <th>Product</th><th>Price</th>
-  //     <tr> 
-  //       <td>React</td>
-  //       <td>320</td>
-  //     </tr>
-  //     <tr>
-  //       <td>React Native</td>
-  //       <td>340</td>
-  //     </tr>
-  //   </table>
-  // );
 }
 
 export default App;
