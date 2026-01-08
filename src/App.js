63d26f4d383926fcccbf791c74190390354b0c6d
pluemkung <39697097+pluemkung@users.noreply.github.com>
2026-01-08 11:42:03 +0700

    EventData2
    EventData2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import './style.css';
-import React from 'react';
-import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
-import Button from './class-components';
-import { Calculator2 } from './calculator';
+import { EventData2 } from './event-data';
+
 
 
 export default function App(){
-  return <Calculator2/>
+  return <EventData2/>
 }
