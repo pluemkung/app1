aa30b7a3478d519fc1ed7ae9d00146a501aac9c9
pluemkung <39697097+pluemkung@users.noreply.github.com>
2026-01-08 11:13:21 +0700

    Calculator2 in Event
    Calculator2 in Event

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import React from 'react';
 import { Content, Footer, Header } from './func-components';
 import Calendar from './class-components';
 import Button from './class-components';
+import { Calculator2 } from './calculator';
 
-function App() {
-  return <Button/>
+
+export default function App(){
+  return <Calculator2/>
 }
-
-export default App;
