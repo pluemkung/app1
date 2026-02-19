206a031558d797033ba976e2d2f4c16e82938bea
pluemkung <39697097+pluemkung@users.noreply.github.com>
2026-02-19 11:46:24 +0700

    form-text
    form-text

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Content2 from './context-content2';
 import Header2 from './context-header2';
 import Router2 from './router2';
 import Router3 from './router3';
+import FormText from './form-text';
 
 
 
 export default function App(){
   // let [user,setUser] = React.useState('')
   return (
-   <Router3/>
+   <FormText/>
   )
 }
