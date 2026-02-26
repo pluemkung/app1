5783722854f55369d8548986658255f855fd62a0
pluemkung <39697097+pluemkung@users.noreply.github.com>
2026-02-26 10:28:22 +0700

    select File

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Header2 from './context-header2';
 import Router2 from './router2';
 import Router3 from './router3';
 import FormText from './form-text';
+import FormSelectFile from './form-select-file';
 
 
 
 export default function App(){
   // let [user,setUser] = React.useState('')
   return (
-   <FormText/>
+   <FormSelectFile/>
   )
 }
